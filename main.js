b85252a0770f19603ae9c70d7e7029274ceb5373
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2022-03-05 16:46:59 +0100

    add reset button

--- main.js
+++ main.js
@@ -6,25 +6,32 @@ const btn = document.createElement("button");
 btn.textContent = "Create list";
 document.body.appendChild(btn);
 
+const btnReset = document.createElement("button");
+btnReset.textContent = "Reset";
+document.body.appendChild(btnReset);
+btnReset.style.marginLeft = "10px";
+
 const ul = document.createElement("ul");
 document.body.appendChild(ul);
 
 btn.addEventListener("click", createLiElements);
+btnReset.addEventListener("click", resetList);
 }
 
 const createLiElements = () => {
-
 for (let i = 1; i < 11; i++) {
-
-    let li = document.createElement("li");
-    document.body.appendChild(li);
+    const li = document.createElement("li");
+    document.querySelector("ul").appendChild(li);
     li.textContent = "Element no. " + orderElement;
     orderElement++;
     li.style.fontSize = size + "px";
     size++;
     li.style.listStyleType = "none";
-    li.style.paddingLeft = "30px";
 }
 }
+const resetList = () => {
+    document.querySelector("ul").textContent = "";
+}
+
 
 init()
